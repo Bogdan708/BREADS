928fa09d70e551c113de8f8736ef9fd907e39f4e
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-07-27 20:58:38 -0500

    finishing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// DEPENDENCIES
 const express = require('express')
 
 // CONFIGURATION
@@ -9,24 +10,25 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+
 // MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
-
-
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
-// 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
-
 
 //Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 // LISTEN
 app.listen(PORT, () => {
--- views/show.jsx
+++ views/show.jsx
@@ -1,33 +1,30 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
-  console.log(bread.name)
-    return (
-      <Default>
-        <h2>Show Page</h2>
-        <h3>{bread.name}</h3>
-        <p>
+function Show ({bread, index}) {
+    // Confirm we are getting our bread data in the terminal.
+    // console.log(bread.name)
+      return (
+        <Default>
+          <h2>Show Page</h2>
+          <h3>{bread.name}</h3>
+          <p>
             and it
             {
-                bread.hasGluten
-                ? <span> does </span>
-                : <span> does NOT </span>
+            bread.hasgluten 
+            ? <span>does</span>
+            :<span> does NOT </span>
             }
             have gluten.
-        </p>
-        <img src={bread.image} alt={bread.name} />
-        <li><a href="/breads">Go home</a></li>
-      </Default>
-    )
-}
+          </p>
+          <form action={`/breads/${index}?_method=DELETE`} method="POST">
+  <input type='submit' value="DELETE"/>
+</form>
+          <img src={bread.image} alt={bread.name} />
+          <li><a href="/breads">Go Home</a></li>
+        </Default>
+      )
+  }
+  
 
 module.exports = Show
-
-
-
-
-
-
-  
-  
